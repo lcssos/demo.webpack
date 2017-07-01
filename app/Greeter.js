1cefd11743220c59b019119f82e01f4f76c14bd6
lcssos <lcs@hh-medic.com>
2017-07-01 22:55:29 +0800

    init

---
+++ app/Greeter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lcssos on 2017/7/1.
+ */
+// Greeter.js
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ app/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by lcssos on 2017/7/1.
+ */
+//main.js
+var greeter = require('./Greeter.js');
+document.getElementById('root').appendChild(greeter());
