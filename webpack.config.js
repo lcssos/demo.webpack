044d2437f6205fe23fce719b2cfa3f8d0ee2c285
lcssos <lcs@hh-medic.com>
2017-07-02 13:58:34 +0800

    angular2

--- webpack.config.js
+++ webpack.config.js
@@ -3,83 +3,99 @@
  * webpack 主配置文件
  */
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-    // 打包的源文件 目标文件
-    entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
-    // output: {
-    //     path: __dirname + "/public",//打包后的文件存放的地方
-    //     filename: "bundle.js"//打包后输出文件的文件名
-    // },
-    output: {
-        path: __dirname + "/public",
-        filename: '[name]-[hash].min.js',
-    },
-
-    //配置生成Source Maps，选择合适的选项(效率依次提高，安全依次降低)
-    //source-map cheap-module-source-map eval-source-map cheap-module-eval-source-map
-    devtool: 'eval-source-map',
-
-    //webpack-dev-server 服务器配置
-    devServer: {
-        contentBase: "./public",//本地服务器所加载的页面所在的目录
-        colors: true,// 设置为true，使终端输出的文件为彩色的
-        historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
-        inline: true, // 设置为true，当源文件改变时会自动刷新页面
-        port: 8080  //设置默认监听端口，如果省略，默认为”8080“
-    },
-
-    module: {
-        //在配置文件里添加JSON loader
-        //test 一个匹配loaders所处理的文件的拓展名的正则表达式（必须）
-        //loader loader的名称（必须） 注意：：必须是全名，带-loader结尾
-        //include/exclude:手动添加必须处理的文件（文件夹）或屏蔽不需要处理的文件（文件夹）（可选）
-
-        //babel
-        rules: [
-            {
-                test: /\.json$/,
-                use: "json-loader"
-            },
-
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-                // options : {
-                //     presets: 'react'
-                // }
-            },
-
-            {
-                test: /\.(css|scss)$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader'],
-                }),
-            },
-        ]
-    },
-
-
-    plugins:[
-        new ExtractTextPlugin('[name]-[hash].min.css'),
-
-        //依据一个简单的模板，帮你生成最终的Html5文件，这个文件中自动引用了你打包后的JS文件。每次编译都在文件名中插入一个不同的哈希值。
-        new HtmlWebpackPlugin({
-            template: 'index.tpl.html',
-            inject: 'body',
-            filename: 'index.html',
-        }),
-    ]
-
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             use: [ 'style-loader', 'css-loader' ]
-    //         }
-    //     ]
-
+switch (process.env.NODE_ENV) {
+    case 'prod':
+    case 'production':
+        module.exports = require('./config/webpack.prod')({env: 'production'});
+        break;
+    case 'test':
+    case 'testing':
+        module.exports = require('./config/webpack.test')({env: 'test'});
+        break;
+    case 'dev':
+    case 'development':
+    default:
+        module.exports = require('./config/webpack.dev')({env: 'development'});
 }
+
+//
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+// module.exports = {
+//     // 打包的源文件 目标文件
+//     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
+//     // output: {
+//     //     path: __dirname + "/public",//打包后的文件存放的地方
+//     //     filename: "bundle.js"//打包后输出文件的文件名
+//     // },
+//     output: {
+//         path: __dirname + "/public",
+//         filename: '[name]-[hash].min.js',
+//     },
+//
+//     //配置生成Source Maps，选择合适的选项(效率依次提高，安全依次降低)
+//     //source-map cheap-module-source-map eval-source-map cheap-module-eval-source-map
+//     devtool: 'eval-source-map',
+//
+//     //webpack-dev-server 服务器配置
+//     devServer: {
+//         contentBase: "./public",//本地服务器所加载的页面所在的目录
+//         colors: true,// 设置为true，使终端输出的文件为彩色的
+//         historyApiFallback: true,//在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+//         inline: true, // 设置为true，当源文件改变时会自动刷新页面
+//         port: 8080  //设置默认监听端口，如果省略，默认为”8080“
+//     },
+//
+//     module: {
+//         //在配置文件里添加JSON loader
+//         //test 一个匹配loaders所处理的文件的拓展名的正则表达式（必须）
+//         //loader loader的名称（必须） 注意：：必须是全名，带-loader结尾
+//         //include/exclude:手动添加必须处理的文件（文件夹）或屏蔽不需要处理的文件（文件夹）（可选）
+//
+//         //babel
+//         rules: [
+//             {
+//                 test: /\.json$/,
+//                 use: "json-loader"
+//             },
+//
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: 'babel-loader',
+//                 // options : {
+//                 //     presets: 'react'
+//                 // }
+//             },
+//
+//             {
+//                 test: /\.(css|scss)$/,
+//                 loader: ExtractTextPlugin.extract({
+//                     fallback: 'style-loader',
+//                     use: ['css-loader', 'postcss-loader'],
+//                 }),
+//             },
+//         ]
+//     },
+//
+//
+//     plugins:[
+//         new ExtractTextPlugin('[name]-[hash].min.css'),
+//
+//         //依据一个简单的模板，帮你生成最终的Html5文件，这个文件中自动引用了你打包后的JS文件。每次编译都在文件名中插入一个不同的哈希值。
+//         new HtmlWebpackPlugin({
+//             template: 'index.tpl.html',
+//             inject: 'body',
+//             filename: 'index.html',
+//         }),
+//     ]
+//
+//     //     rules: [
+//     //         {
+//     //             test: /\.css$/,
+//     //             use: [ 'style-loader', 'css-loader' ]
+//     //         }
+//     //     ]
+//
+// }
